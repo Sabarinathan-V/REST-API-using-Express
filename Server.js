6066b5dea820a1f4b4819da81377e63a5f54768b
Sabarinathan <sabarivanam@gmail.com>
2023-04-19 17:59:53 +0530

    Adding Cors middleware

--- Server.js
+++ Server.js
@@ -3,10 +3,14 @@ require('dotenv').config();
 const app = express();
 const mongoose = require('mongoose');
 const taskRoutes = require('./Routes/TaksRoutes')
+const cors = require('cors');
 
 
 // MIDDLEWARE TO HANDLE JSON DATA
 app.use(express.json());
+
+//MIDDLEWARE TO ENABLE CORS
+app.use(cors());
 
 // DATABASE CONNECTION
 mongoose.connect(process.env.MONGODB_URI).then(
